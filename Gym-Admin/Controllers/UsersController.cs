6f822f330e949d5c627f37d2a3778eb536098b8e
Derric <realbenneh@gmail.com>
2020-06-30 17:20:18 +0000

    Initial User  model and UserController

---
+++ Gym-Admin/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gym_Admin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym_Admin.Controllers
+{
+    [Route("api/Users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+       static List<User> _users = new List<User>()
+        {
+            new User(){ UserId = 0, FirstName ="derrick", LastName="benneh", PhoneNumber=0247891306},
+            new User(){ UserId = 1, FirstName ="desmond", LastName="benneh", PhoneNumber=0247649766},
+            new User(){ UserId = 2, FirstName ="joyce", LastName="benneh", PhoneNumber=0247676766}
+        };
+
+       public IActionResult Get()
+        {
+            return Ok(_users);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]User user)
+        {
+              _users.Add(user);
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPut("{id}")]
+        public void Put( int id, [FromBody] User user)
+        {
+            _users[id] = user;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _users.RemoveAt(id);
+        }
+    }
+}
---
+++ Gym-Admin/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gym_Admin.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int PhoneNumber { get; set; }
+      
+    }
+}
